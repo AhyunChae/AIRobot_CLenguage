6641e1b554037d05a8cbe500295fbb1c982e4caa
Ahyun <codkgus0223@gmail.com>
2025-10-30 11:12:15 +0900

    day11

--- Day10/6.로그파일.c
+++ Day10/6.로그파일.c
@@ -2,8 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define BUFFER_LEN 100
+
+typedef struct node {
+  char* data;
+  Node* next;
+}Node;
+
+typedef struct block_list {
+  Node* top;
+  int len;
+} BlockList;
+
+
+BlockList* create_list(void);
+void push_back(BlockList *list, char *str);
 
 int main(void) {
+  int i, j;
   const FILE *log;
 
   log = fopen("../battery-report-test.html", "r");
@@ -15,7 +31,10 @@ int main(void) {
   FILE *fp = log;
   fseek(fp, 0, SEEK_END);
   long int size = ftell(fp);
-  char* target = strstr(fp, "<td class=\"dateTime\">");
+  
+  char buffer[BUFFER_LEN];
+  char* target = strstr(buffer, "<td class=\"dateTime\">");
+  BlockList* block = create_list();
   
 
 
@@ -23,3 +42,30 @@ int main(void) {
   return 0;
 }
 
+BlockList* create_list(void) {
+  BlockList list;
+  list.len = 1;
+
+  return &list;
+}
+
+Node* find_tail(Node* node) {
+  if (node->next == -1) {
+    return node;
+  }
+  return find_tail(node->next);
+}
+
+void push_back(BlockList *list, char *str) {
+  Node node;
+  node.data = str;
+  node.next = -1;
+
+  if (list->len == 1) {
+    list->top = &node;
+  }
+  else {
+    Node* tail = find_tail(list->top);
+    tail->next = &node;
+  }
+}
